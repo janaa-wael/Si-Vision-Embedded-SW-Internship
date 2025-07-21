c5b1fa546bafd8a8da5483e121d16fc18f4f9141
janaa-wael <janawael119@gmail.com>
2025-07-21 16:02:03 +0300

    Adding SystemWakeup Concrete Class files

---
+++ Events Manager Project/Events_Manager_Implementation/SystemWakeup.cpp
@@ -0,0 +1,27 @@
+/*
+ * SystemWakeup.cpp
+ *
+ *  Created on: Jul 21, 2025
+ *      Author: hp
+ */
+
+#include "SystemWakeup.h"
+#include <iostream>
+using namespace std;
+
+SystemWakeup::SystemWakeup() {
+	cout << "System Wakeup constructor is called" << endl;
+
+}
+
+void SystemWakeup::executeEvent()
+{
+	setUpSystem();
+	cout << "System successfully transitioned to Wakeup mode" << endl;
+
+}
+
+SystemWakeup::~SystemWakeup() {
+	cout << "System Wakeup Destructor is called" << endl;
+}
+
---
+++ Events Manager Project/Events_Manager_Implementation/SystemWakeup.h
@@ -0,0 +1,19 @@
+/*
+ * SystemWakeup.h
+ *
+ *  Created on: Jul 21, 2025
+ *      Author: hp
+ */
+
+#ifndef SYSTEMWAKEUP_H_
+#define SYSTEMWAKEUP_H_
+#include "SystemEvent.h"
+
+class SystemWakeup : public SystemEvent{
+public:
+	SystemWakeup();
+	void executeEvent() override;
+	virtual ~SystemWakeup();
+};
+
+#endif /* SYSTEMWAKEUP_H_ */
