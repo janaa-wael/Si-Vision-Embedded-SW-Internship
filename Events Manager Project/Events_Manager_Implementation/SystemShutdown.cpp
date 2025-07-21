3ad404a4cd5abd4a0fd10c64b7abe3905d2774e9
janaa-wael <janawael119@gmail.com>
2025-07-21 15:56:28 +0300

    Adding SystemShutdown Concerte Class files

---
+++ Events Manager Project/Events_Manager_Implementation/SystemShutdown.cpp
@@ -0,0 +1,25 @@
+/*
+ * SystemShutdown.cpp
+ *
+ *  Created on: Jul 21, 2025
+ *      Author: hp
+ */
+
+#include "SystemShutdown.h"
+#include <iostream>
+using namespace std;
+
+SystemShutdown::SystemShutdown()
+{
+	cout << "System Shutdown Constructor is called" << endl;
+}
+
+void SystemEvent::executeEvent()
+{
+	setUpSystem();
+	cout << "System successfully transition to the Shutdown mode" << endl;
+}
+SystemShutdown::~SystemShutdown() {
+	cout << "System Shutdonw Desructor is called" << endl;
+}
+
---
+++ Events Manager Project/Events_Manager_Implementation/SystemShutdown.h
@@ -0,0 +1,18 @@
+/*
+ * SystemShutdown.h
+ *
+ *  Created on: Jul 21, 2025
+ *      Author: hp
+ */
+
+#ifndef SYSTEMSHUTDOWN_H_
+#define SYSTEMSHUTDOWN_H_
+#include "SystemEvent.h"
+
+class SystemShutdown {
+public:
+	SystemShutdown();
+	virtual ~SystemShutdown();
+};
+
+#endif /* SYSTEMSHUTDOWN_H_ */
